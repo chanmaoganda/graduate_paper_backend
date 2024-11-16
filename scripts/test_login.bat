dc8d4cfdd256c34ce99a944b78ad4feb61ea11e2
chanmaoganda <avaniawang@outlook.com>
2024-11-17 01:33:22 +0800

    add test script

---
+++ scripts/test_login.bat
@@ -0,0 +1,9 @@
+cargo test teacher_register_test
+cargo test student_register_test
+
+cargo test login_test
+cargo test list_test
+cargo test query_test
+
+cargo test teacher_unregister_test
+cargo test student_unregister_test
